342c4a90cb2b2cb4d723950a06808a7060b98255
LucasNF22 <lucasnfiorentino@gmail.com>
2024-10-15 14:28:39 -0300

    Creacion de página 404

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,24 @@
+import { notFound } from "next/navigation";
 
-export default function () {
+
+interface Props {
+  params: {
+    id: string,
+
+  }
+}
+
+export default function ({ params }: Props ) {
+
+  const { id } = params;
+
+  if ( id === 'kids' ) {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Cart page</h1>
+      <h1 className="tetbold">Cart page { id } </h1>
     </div>
   );
 }
