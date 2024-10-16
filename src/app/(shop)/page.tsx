21f0d9925226fb50dd646c09e36139f694384ea1
LucasNF22 <lucasnfiorentino@gmail.com>
2024-10-16 16:53:33 -0300

    Elaboración de gris products, efectos mouseOver

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,6 +1,10 @@
-import { Title } from "@/components";
-import { titleFont } from "@/config/fonts";
-import Image from "next/image";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+
+const products = initialData.products;
+
+
 
 export default function Home() {
   return (
@@ -11,6 +15,9 @@ export default function Home() {
         className="mb-2"
       />
 
+      <ProductGrid 
+        products={ products }
+      />
       
     </>
   );
