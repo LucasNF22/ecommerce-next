aa5517363ebbcfb1f50f68c111ebbb98844d2253
LucasNF22 <lucasnfiorentino@gmail.com>
2024-10-21 16:35:57 -0300

    Continuando slidewhow enproducto

--- src/components/product/product-slideshow/ProductSlideshow.tsx
+++ src/components/product/product-slideshow/ProductSlideshow.tsx
@@ -30,8 +30,8 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
                 style={{
                     '--swiper-navigation-color': '#fff',
                     '--swiper-pagination-color': '#fff',
-                } as React.CSSProperties }
-                
+                } as React.CSSProperties}
+
                 spaceBetween={10}
                 navigation={true}
                 thumbs={{ swiper: thumbsSwiper }}
@@ -39,19 +39,59 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
                 className="mySwiper2"
             >
                 {
-                    images.map( image => (
-                        <SwiperSlide key={ image }>
-                            <Image 
+                    images.map(image => (
+                        <SwiperSlide key={image}>
+                            <Image
                                 width={1024}
                                 height={800}
-                                src={`/products/${ image }`}
-                                alt={ title }
+                                src={`/products/${image}`}
+                                alt={title}
                                 className='rounded-md'
                             />
                         </SwiperSlide>
                     ))
                 }
-               </Swiper>
+            </Swiper>
+            <Swiper
+                onSwiper={setThumbsSwiper}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 }
