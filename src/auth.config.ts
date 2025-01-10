04d89d26b35172a4f206b541db5536c6cf84eec5
LucasNF22 <lucasnfiorentino@gmail.com>
2025-01-10 11:22:38 -0300

    Configuracion inicial de credenciales para el auth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,12 +1,35 @@
-import NextAuth from "next-auth"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    pages:{
-        signIn: '/auth/login',
-        newUser: '/auth/new-account'
-    },
-    providers: [
-        //proveedores
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
 
-    ],
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    newUser: "/auth/new-account",
+  },
+  providers: [
+    //proveedores
+
+    Credentials({
+      async authorize(credentials) {
+        
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+          if( !parsedCredentials.success )return null;
+
+          const { email, password } = parsedCredentials.data;
+
+          console.log({email, password});
+          
+
+        //buscar el correo
+
+        //Comparar las contraseñas
+
+        return null;
+      },
+    }),
+  ],
 });
