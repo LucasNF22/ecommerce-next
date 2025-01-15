aafaafb668b565678ff6c998509b9527425ea663
LucasNF22 <lucasnfiorentino@gmail.com>
2025-01-15 15:48:39 -0300

    Modificaciones de ayuda visual y experiencia de usuario en pantalla de login

--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import Link from "next/link"
-import { useFormState } from "react-dom"
+import { useFormState, useFormStatus } from "react-dom"
 import { authenticate } from "@/actions"
+import { IoInformationOutline } from "react-icons/io5"
 
 export const LoginForm = () => {
 
@@ -27,16 +28,19 @@ export const LoginForm = () => {
                 className="px-5 py-2 border bg-gray-200 rounded mb-5"
                 type="password" 
                 name="password"
-                />
+            />
 
-            <button
-                type="submit"
-                className="btn-primary">
-                Ingresar
-            </button>
+            { state === "credentialsSignin" && (
+                <div className="mb-2 flex flex-row ">
+                    <IoInformationOutline className="h-5 w-5 text-red-500" />
+                    <p className="text-sm text-red-500">Los datos ingresados no son correctos</p>
+                </div>
+            )}
+
+            <LoginButton />
 
 
-            {/* divisor l ine */}
+            {/* divisor line */}
             <div className="flex items-center my-5">
                 <div className="flex-1 border-t border-gray-500"></div>
                 <div className="px-2 text-gray-800">O</div>
@@ -50,5 +54,19 @@ export const LoginForm = () => {
             </Link>
 
         </form>
+    )
+}
+
+
+function LoginButton() {
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+        type="submit"
+        className="btn-primary">
+        Ingresar
+        </button>
+
     )
 }
